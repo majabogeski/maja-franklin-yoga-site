1aaef35005342af067475e3f5d2909c88d9b9df2
Maja Franklin <bogeski.maja@gmail.com>
2025-06-23 09:39:06 -0700

    add Google Tag Manager

--- pages/_document.js
+++ pages/_document.js
@@ -1,3 +1,4 @@
+
 // pages/_document.js
 import { Html, Head, Main, NextScript } from 'next/document'
 
@@ -9,11 +10,32 @@ export default function Document() {
           href="https://assets.calendly.com/assets/external/widget.css"
           rel="stylesheet"
         />
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id=GTM-TL7TXQ7Z'+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TL7TXQ7Z');`,
+          }}
+        />
       </Head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TL7TXQ7Z"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+
         <Main />
         <NextScript />
       </body>
     </Html>
   )
 }
+
